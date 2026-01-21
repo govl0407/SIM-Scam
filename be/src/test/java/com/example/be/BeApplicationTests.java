2a0fe091cee6153bfea17c0e678c62274469638a
최정민 <govl0407@gmail.com>
2026-01-22 00:24:49 +0900

    [repair] be re-init

--- be/src/test/java/com/example/be/BeApplicationTests.java
+++ be/src/test/java/com/example/be/BeApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.simscam;
+package com.example.be;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SimScamApplicationTests {
+class BeApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
