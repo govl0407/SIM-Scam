497cbee7023c8e1a0474688869ae28290936083d
최정민 <govl0407@gmail.com>
2026-01-22 00:01:40 +0900

    [feature] be init

---
+++ BE/SimScam/src/main/java/com/example/simscam/SimScamApplication.java
@@ -0,0 +1,13 @@
+package com.example.simscam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimScamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimScamApplication.class, args);
+    }
+
+}
