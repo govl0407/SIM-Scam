669c77b60071932a6de2b832c857a62d1387c1b2
HyeonJu Kwon <135528113+HyeonJu-Kwon@users.noreply.github.com>
2026-01-22 20:11:21 +0900

    feat: vue-spring 연동

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,30 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const message = ref('로딩중...')
+
+onMounted(async () => {
+  try {
+    const res = await axios.get('http://localhost:8080/api/hello')
+    message.value = res.data   // 문자열이면 그대로
+  } catch (e) {
+    message.value = '백엔드 연결 실패'
+    console.error(e)
+  }
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <h1>SIM-Scam</h1>
+    <p> 안녕하시렵니까</p>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+main {
+  padding: 40px;
+  font-family: Arial, sans-serif;
 }
 </style>
