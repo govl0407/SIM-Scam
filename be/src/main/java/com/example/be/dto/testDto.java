fb91d0a534fd7c9473114192160ed0736c9fb59b
최정민 <govl0407@gmail.com>
2026-01-22 22:26:45 +0900

    [be][test] get,post test api

---
+++ be/src/main/java/com/example/be/dto/testDto.java
@@ -0,0 +1,13 @@
+package com.example.be.dto;
+
+public class testDto {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
