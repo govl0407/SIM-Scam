b883079c4db1d234b8e77e170959e4f303ddc93c
최정민 <govl0407@gmail.com>
2026-01-22 23:17:58 +0900

    [be][test] gpt test

--- be/src/main/java/com/example/be/controller/testController.java
+++ be/src/main/java/com/example/be/controller/testController.java
@@ -1,11 +1,14 @@
 package com.example.be.controller;
 
 import com.example.be.dto.testDto;
+import com.example.be.service.gptService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*") // 프론트 테스트용 (나중에 제한)
 public class testController {
+    private final gptService gptService;
+
     @PostMapping("/test/chat")
     public void receiveChat(@RequestBody testDto request) {
         System.out.println("📩 받은 메시지: " + request.getMessage());
@@ -14,4 +17,16 @@ public class testController {
     public String testGet(){
         return "hello";
     }
+
+    public testController(gptService gptService) {
+        this.gptService = gptService;
+    }
+
+    @GetMapping("/test/gpt")
+    public String testGpt() {
+        String fixedMessage = "너는 지금 뭐하고 있어?";
+        return gptService.textGpt(fixedMessage);
+    }
+
 }
+
