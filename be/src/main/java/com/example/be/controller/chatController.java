511f5d462af55b51643d3f4533c6a76c6745cfa7
최정민 <govl0407@gmail.com>
2026-02-05 23:44:43 +0900

    [be][feature] 이벤트 로그 api추가

--- be/src/main/java/com/example/be/controller/chatController.java
+++ be/src/main/java/com/example/be/controller/chatController.java
@@ -2,10 +2,13 @@ package com.example.be.controller;
 
 import com.example.be.dto.EventResponseDto;
 import com.example.be.dto.userMessageDto;
+import com.example.be.service.test.testChatMemory;
 import com.example.be.service.test.testChatService;
 import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -15,9 +18,11 @@ public class chatController {
 
     private final testChatService chatService;
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final com.example.be.service.test.testChatMemory testChatMemory;
 
-    public chatController(testChatService chatService) {
+    public chatController(testChatService chatService, testChatMemory testChatMemory) {
         this.chatService = chatService;
+        this.testChatMemory = testChatMemory;
     }
 
     @PostMapping("/message")
@@ -45,5 +50,23 @@ public class chatController {
             throw new RuntimeException("GPT 응답 JSON 파싱 실패: " + strJson, e);
         }
 
+    }
+    @GetMapping("/event-logs")
+    public Map<String, Object> getEventLogs(
+            @RequestParam String sessionId
+    ) {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("sessionId", sessionId);
+        response.put(
+                "currentEvent",
+                testChatMemory.getCurrentEvent(sessionId)
+        );
+        response.put(
+                "eventLogs",
+                testChatMemory.getEventLogs(sessionId)
+        );
+
+        return response;
     }
 }
